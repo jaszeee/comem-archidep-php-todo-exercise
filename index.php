5a95cca46b6b1fa51b406a8f423723fe345f8fe5
Jasmine Eswaran <jasmine.eswaran@heig-vd.ch>
2022-11-09 12:41:11 +0100

    Update configuration

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/comem-archidep-php-todo-exercise/');
+define('BASE_URL', '/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
 define('DB_PASS', 'nD$054kfkuWW');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '8889');
+define('DB_PORT', '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
